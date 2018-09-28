37d22c6f0a01e14d87fc47058876dd39b3055237
mrloop <ewan@mrloop.com>
2018-09-28 19:49:52 +0100

    Minimal breaking amd import
    Where qz-tray factory is an object not a function
    
    Loading the ember app in a browser results in the following error
    
        TypeError: this.callback.apply is not a function[Learn More]
    
        Module.prototype.exports  loader.js:106
        Module.prototype._reify   loader.js:143
        Module.prototype.reify    loader.js:130
        Module.prototype.exports  loader.js:104
        requireModule             loader.js:27
        <anonymous>               app-boot.js:3
    
    Editing the qz-tray lib to use a factory function rather then factory
    object allows the app to load.
    
    Looking at https://github.com/amdjs/amdjs-api/blob/master/AMD.md#factory-
    a function or object should be allowed.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,9 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('node_modules/qz-tray/qz-tray.js', {
+    using: [{ transformation: 'amd', as: 'qz-tray' }],
+  });
+
   return app.toTree();
 };
